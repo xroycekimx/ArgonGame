ea1d5aab41fee501979b4f44a5466e5494dbe904
Jemma Yang <jessicyoungvive@163.com>
2017-04-17 13:27:24 -0400

    test functions

--- game/test page/aframe.js
+++ game/test page/aframe.js
@@ -2,8 +2,10 @@ var nowFocused = "";
 var rotZ = 0;
 var scaleNum = 1;
 var score = 0;
-var alienNum = 20;
-var bulletNum = 20;
+var alienNum = 15;
+var bulletNum = 15;
+
+var secondLocation = [false, false, false];
 
 function main() {
 
@@ -11,7 +13,7 @@ function main() {
     init: function () {
 
       this.el.addEventListener('mouseenter', function (evt) {
-        console.log("focus");
+        // console.log("focus");
         this.setAttribute('material', 'opacity', 0.1);
         nowFocused = this.id;
         rotZ = this.getAttribute('rotation').z;       
@@ -47,16 +49,19 @@ function playGunshot() {
    if(nowFocused != ""){ 
        var focusedObjtect = document.getElementById(nowFocused);
 
-       if (rotZ == 200){
+       if (rotZ == 80){
             console.log(rotZ);
             focusedObjtect.parentNode.removeChild(focusedObjtect);
             alienNum = alienNum -1;
-            score += 100;
+            score = score + 10;
+
+            var pointsAudio = document.getElementById("gotPonitsAudio");
+            pointsAudio.play();
         }
         
         else{
-            rotZ += 100;
-            scaleNum = scaleNum - 0.15;
+            rotZ += 40;
+            scaleNum = scaleNum - 0.1;
             console.log("shoot");
            
              // ROTATION   
@@ -70,18 +75,32 @@ function playGunshot() {
 
   updateGUI();
   checkBullets();
+  leadNext();
 }
 
 function updateGUI(){
+
+  if (score < 10){
+    var nodeScore = document.getElementById("scoreArea");
+    nodeScore.innerHTML = "<div>00" + score + "</div>"; 
+  }
   
-  var nodeScore = document.getElementById("scoreArea");
-  nodeScore.innerHTML = "<div>" + score + "</div>"; 
+  else if (score < 100){
+     var nodeScore = document.getElementById("scoreArea");
+     nodeScore.innerHTML = "<div>0" + score + "</div>"; 
+  }
+
+  else{
+    var nodeScore = document.getElementById("scoreArea");
+    nodeScore.innerHTML = "<div>" + score + "</div>"; 
+  }
+  
 
   var nodeAlien = document.getElementById("alienArea");
-  nodeAlien.innerHTML = "<div>" + alienNum+ "/20</div>";
+  nodeAlien.innerHTML = "<div>" + alienNum+ "/15</div>";
 
   var nodeBullet = document.getElementById("bulletArea");  
-  nodeBullet.innerHTML = "<div>" + bulletNum+ "/20</div>";
+  nodeBullet.innerHTML = "<div>" + bulletNum+ "/15</div>";
   
 }
 
@@ -94,7 +113,10 @@ function checkBullets(){
 }
 
 function reload(){
-  bulletNum = 20;
+  var reloadAudio = document.getElementById("reloadAudio");
+  reloadAudio.play();
+
+  bulletNum = 15;
   var showButton = document.getElementById("button");
   showButton.innerHTML = "<img id='bt' src='../../resources/images/button.png' width='50px' height='100px' onclick='playGunshot()'>";
   console.log("reload bullets");
@@ -102,4 +124,31 @@ function reload(){
   var nodeBullet = document.getElementById("bulletArea");  
   nodeBullet.innerHTML = "<div>" + bulletNum+ "/20</div>";
 }
+
+function leadNext(){
+  var alien01 = document.getElementsByClassName("alien01");
+  console.log(alien01.length);
+
+  var alien02 = document.getElementsByClassName("alien02");
+  console.log(alien02.length);
+
+  var alien03 = document.getElementsByClassName("alien03");
+  console.log(alien03.length);
+
+  var alien = [alien01.length, alien02.length, alien03.length];
+  var next = ["Student Center.","library","Culc"]
+
+  for (i = 0; i < alien.length; i++) { 
+    if (alien[i] == 0) {
+      window.alert("Go to " + next[i] +".");
+      secondLocation[i] = true;
+      }
+    }
+}
     
+
+function ShowSecond{
+  if (secondLocation[0] ==true){
+           
+  }
+}
